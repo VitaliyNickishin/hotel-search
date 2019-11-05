eb8723cba3162cfcbac86d795ff7fed527e5db31
“VitaliyNickishin” <nickishin34@gmail.com>
2019-11-05 16:07:53 +0200

    create Hero, Banner

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,17 @@
 import React from 'react'
+import Hero from '../components/Hero/Hero'
+import Banner from '../components/Banner/Banner'
 
 export default function Home() {
  return (
   <div>
-   
+   <Hero>
+    <Banner
+     title="Choice your place"
+    >
+
+    </Banner>
+   </Hero>
   </div>
  )
 }
